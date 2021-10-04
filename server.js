bbf0700a56c3f3e8ca4cae56c4d329e5dd623071
Vivek Mishra <v.mishra@f5.com>
2021-10-04 21:45:31 +0530

    working with content lenght

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
-const http = require('http');
-Busboy = require('busboy')
-const express = require('express')
-const path = require('path')
-var FormData = require('form-data');
-
-const app = express()
-
-var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+let express = require('express');
+let app = express();
+var multiparty = require("multiparty");
+var request = require("request");
 
 app.use(express.static('public', {
     etag: true, // Just being explicit about the default.
@@ -20,62 +13,41 @@ app.use(express.static('public', {
       }
     },
   }))
-// Disable cache
-app.set('etag', false)
-app.use((req, res, next) => {
-    res.set('Cache-Control', 'no-store')
-    next()
-})
 
 app.get('/health', (req, res) => {
     return res.send('ok')
 })
-app.get('/data', (req,res) => {
-    const count = 1;
-    res.send(
-        {
-            country: 'India',
-            city: 'Hyderabad',
-            state: 'Telangana',
-            zipCode: '500075'
-          }
-      );
-})
 
-app.post( '/upload', function ( req, res ) {
-    console.log("Request received by proxy");
-    console.log(req);
-    var busboy = new Busboy( { headers: req.headers } );
-    var formData = new FormData();
+app.post( '/upload', function ( req, res, next ) {
+    var form = new multiparty.Form();
 
-    busboy.on('file', function(fieldname, file) {
-        console.log(fieldname);
-        console.log(file);
-        formData.append('file', file);
-        formData.append('fileName', fieldname);
-        
-        var proxyReq = http.request({
-            headers: req.headers,
-            host: '127.0.0.1',
-            port: 3001,
-            method: 'POST',
-            path: '/upload'
-        }, function(response) {
-            response.resume();
-        });
-        proxyReq.on('error', e => {
-            console.log(e);
-        });
-        file.pipe(proxyReq);
-    });
+    form.on("part", function(part){
+        if(part.filename) {
+            var formData = {
+                thumbnail: {
+                    value:  part,
+                    options: {
+                        filename: part.filename,
+                        contentType: part["content-type"],
+                        knownLength: 244
+                    }
+                }
+            };
 
-    busboy.on( 'finish', function () {
-        console.log('busboy finished');
-        req.body = formData;
-        res.send(formData)
-    });
+            request.post({url:'http://localhost:3001/upload', formData: formData}, function (err, httpResponse, body) {
+                if(err) {
+                    return console.error('upload failed:', err);
+                }
+                console.log('Upload successful!  Server responded with:');
+            });
+        }
+    })
 
-    busboy.end(req.rawBody);
+    form.on("error", function(error){
+        console.log(error);
+    })
+
+    form.parse(req); 
 });
 
 app.set('port', process.env.PORT || 3000);
